cc85a0e83c47e6cb21a5e849f090bc290c94ac5b
Liia Zubova <vardlitmo@gmail.com>
2022-12-22 22:51:24 +0300

    dist

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,15 +1,6 @@
 //first task
 //чтобы экспортировать код, который здесь есть
 const modals = () => {
-<<<<<<< HEAD
-  function bindModal(triggerSelector, modalSelector, closeSelector) {
-    //на несколько одинаковых элементов повесить одни и те же функции
-    const trigger = document.querySelectorAll(triggerSelector),
-      modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector);
-
-    trigger.forEach(item => {
-=======
   function bindModal(triggersSelector, modalSelector, closeSelector) {
     //на несколько одинаковых элементов повесить одни и те же функции
     const triggers = document.querySelectorAll(triggersSelector);
@@ -22,17 +13,12 @@ const modals = () => {
     };
 
     triggers.forEach((item) => {
->>>>>>> c6ee0741e80ab3f337ac95d82a7ae7c9f28c9abe
       item.addEventListener('click', (e) => {
         if (e.target) {
           e.preventDefault();
         }
         //модальное окно показывается на странице
-<<<<<<< HEAD
-        modal.style.display = "block";
-=======
         modal.style.display = 'block';
->>>>>>> c6ee0741e80ab3f337ac95d82a7ae7c9f28c9abe
         //когда модальное окно открыто, то скролится только модальное окно
         document.body.style.overflow = 'hidden';
         //чтоб использовать класс
@@ -42,53 +28,19 @@ const modals = () => {
 
     //модальное окно закрывается при нажатии на крестик
     close.addEventListener('click', () => {
-<<<<<<< HEAD
-			 modal.style.display = 'none';
-			 document.body.style.overflow = '';
-			 //чтоб использовать класс
-			 // document.body.classList.remove('modal-open');
-		 });
-=======
       closeModal();
       //чтоб использовать класс
       // document.body.classList.remove('modal-open');
     });
->>>>>>> c6ee0741e80ab3f337ac95d82a7ae7c9f28c9abe
 
     //чтоб мод окно закрывалось при нажатии вне модального окна
     modal.addEventListener('click', (e) => {
       if (e.target === modal) {
-<<<<<<< HEAD
-        modal.style.display = 'none';
-        document.body.style.overflow = '';
-=======
         closeModal();
->>>>>>> c6ee0741e80ab3f337ac95d82a7ae7c9f28c9abe
         //чтоб использовать класс
         // document.body.classList.remove('modal-open');
       }
     });
-<<<<<<< HEAD
-  }
-
-  function showModalByTime(selector, time){
-	setTimeout(() => {
-		document.querySelector(selector).style.display = 'block';
-		document.body.style.overflow = 'hidden';
-	}, time);
-  }
-
-//   const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
-//     //модальное окно
-//     modalEngineer = document.querySelector('.popup_engineer'),
-//     //крестик, который внутри модального окна
-//     modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
-
-  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-
-  bindModal('.phone_link', '.popup', '.popup .popup_close');
-//   showModalByTime('.popup', 60000);
-=======
 
     //модальное окно закрывается при нажатии на escape
     document.addEventListener('keydown', (e) => {
@@ -119,7 +71,6 @@ const modals = () => {
 
   bindModal('.phone_link', '.popup', '.popup .popup_close');
   showModalByTime('.popup', 3000);
->>>>>>> c6ee0741e80ab3f337ac95d82a7ae7c9f28c9abe
 };
 
 export default modals;
