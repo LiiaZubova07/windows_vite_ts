9f4d303c6ec26d78c3c21906b7440aa307b298b7
Liia Zubova <vardlitmo@gmail.com>
2022-12-22 16:22:50 +0300

    create: tabs.js

---
+++ src/js/modules/modals.js
@@ -0,0 +1,62 @@
+//first task
+//чтобы экспортировать код, который здесь есть
+const modals = () => {
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    //на несколько одинаковых элементов повесить одни и те же функции
+    const trigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector),
+      close = document.querySelector(closeSelector);
+
+    trigger.forEach(item => {
+      item.addEventListener('click', (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
+        //модальное окно показывается на странице
+        modal.style.display = "block";
+        //когда модальное окно открыто, то скролится только модальное окно
+        document.body.style.overflow = 'hidden';
+        //чтоб использовать класс
+        // document.body.classList.add('modal-open');
+      });
+    });
+
+    //модальное окно закрывается при нажатии на крестик
+    close.addEventListener('click', () => {
+			 modal.style.display = 'none';
+			 document.body.style.overflow = '';
+			 //чтоб использовать класс
+			 // document.body.classList.remove('modal-open');
+		 });
+
+    //чтоб мод окно закрывалось при нажатии вне модального окна
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+        //чтоб использовать класс
+        // document.body.classList.remove('modal-open');
+      }
+    });
+  }
+
+  function showModalByTime(selector, time){
+	setTimeout(() => {
+		document.querySelector(selector).style.display = 'block';
+		document.body.style.overflow = 'hidden';
+	}, time);
+  }
+
+//   const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
+//     //модальное окно
+//     modalEngineer = document.querySelector('.popup_engineer'),
+//     //крестик, который внутри модального окна
+//     modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+
+  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
+//   showModalByTime('.popup', 60000);
+};
+
+export default modals;
