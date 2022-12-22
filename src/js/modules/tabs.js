27ab5ed119367a43d94d4ba1344bbb3e793feb4d
Liia Zubova <vardlitmo@gmail.com>
2022-12-22 18:41:05 +0300

    add: const header, tab, content

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,5 +1,9 @@
-const tabs = () =>{
+const tabs = (headerSelector, TabSelector, ContentSelector, activeClass) => {
+  const header = document.querySelector(headerSelector);
+  const tab = document.querySelectorAll(tabSelector);
+  const content = document.querySelectorAll(contentSelector);
 
+  
 };
 
 export default tabs;
