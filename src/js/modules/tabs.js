e6fe8795a96c22ecc93923118662ed9dde534a6a
Liia Zubova <vardlitmo@gmail.com>
2022-12-22 19:07:54 +0300

    add: function hideTabContent & showTabContent

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,9 +1,31 @@
 const tabs = (headerSelector, TabSelector, ContentSelector, activeClass) => {
-  const header = document.querySelector(headerSelector);
-  const tab = document.querySelectorAll(tabSelector);
-  const content = document.querySelectorAll(contentSelector);
+  const headers = document.querySelector(headerSelector);
+  const tabs = document.querySelectorAll(tabSelector);
+  const contents = document.querySelectorAll(contentSelector);
 
-  
+  const hideTabContent = () => {
+    contents.forEach((content) => {
+      content.style.display = 'none';
+    });
+
+	    //при клике на таб, меняется класс активности у этого таба
+		 tabs.forEach(tab => {
+			tab.classList.remove(activeClass);
+		  });
+  };
+
+
+  //отследит на какой по счёту таб нажал пользователь
+  const showTabContent = (i = 0) => {
+	contents[i].style.display = 'block';
+	tabs[i].classList.add(activeClass);
+  };
+
+  hideTabContent();
+  showTabContent();
+
 };
+ 
+
 
 export default tabs;
