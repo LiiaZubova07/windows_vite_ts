f3b406c9cf57115f19e3c482780a0600960fa0c8
Liia Zubova <vardlitmo@gmail.com>
2022-12-22 22:54:01 +0300

    dist

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 import './slider';
-<<<<<<< HEAD
 import modals from './modules/modals';
 import tabs from './modules/tabs';
 
@@ -12,11 +11,4 @@ window.addEventListener('DOMContentLoaded', () => {
     '.decoration_content > div > div',
     'after_click'
   );
-=======
-import modals from './modules/modals.js';
-
-//выполняется тогда, когда ДОМ-структура готова
-window.addEventListener('DOMContentLoaded', () => {
-  modals();
->>>>>>> c6ee0741e80ab3f337ac95d82a7ae7c9f28c9abe
 });
