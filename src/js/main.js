3c072185b574517ccef6e20da72da2af3182b16d
Liia Zubova <vardlitmo@gmail.com>
2022-12-23 22:36:19 +0300

    add: tabs

--- src/js/main.js
+++ src/js/main.js
@@ -3,29 +3,26 @@ import modals from './modules/modals.js';
 import tabs from './modules/tabs.js';
 import forms from './modules/forms.js';
 
-// const tabsGlazing = () => {
-//   '.glazing_slider', '.glazing_block', '.glazing_content', 'active';
-// };
 
-// const tabsDecoration = () => {
-//   '.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click';
-// };
-
-// const TabsGlazing = ('.glazing_slider', '.glazing_block', '.glazing_content', 'active') => {};
+const tabsGlazing = ({ glazing_slider, glazing_block, glazing_content, active }) => {};
 
 //выполняется тогда, когда ДОМ-структура готова
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
   modals();
-    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
 
-    tabs(
-      '.decoration_slider',
-      '.no_click',
-      '.decoration_content > div > div',
-      'after_click'
-    );
-//   tabsGlazing();
-//   tabsDecoration();
+  tabs({
+	headerSelector: '.glazing_slider',
+	tabSelector: '.glazing_block',
+	contentSelector: '.glazing_content',
+	activeClass: 'active'});
+
+   tabs({
+		headerSelector: '.decoration_slider',
+		tabSelector: '.no_click',
+		contentSelector: '.decoration_content > div > div',
+		activeClass: 'after_click'
+	});
+
   forms();
 });
