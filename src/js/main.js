8e76808e5b5d0bd62ea6f6c9f833814e964b0e1f
Liia Zubova <vardlitmo@gmail.com>
2022-12-23 18:59:31 +0300

    add: tabs.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,11 @@ import modals from './modules/modals.js';
 //выполняется тогда, когда ДОМ-структура готова
 window.addEventListener('DOMContentLoaded', () => {
   modals();
+  tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+  tabs(
+    '.decoration_slider',
+    '.no_click',
+    '.decoration_content > div > div',
+    'after_click'
+  );
 });
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,47 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+	const headers = document.querySelector(headerSelector);
+	const tabs = document.querySelectorAll(tabSelector);
+	const contents = document.querySelectorAll(contentSelector);
+ 
+	const hideTabContent = () => {
+	  contents.forEach((content) => {
+		 content.style.display = 'none';
+	  });
+ 
+	  //при клике на таб, меняется класс активности у этого таба
+	  tabs.forEach((tab) => {
+		 tab.classList.remove(activeClass);
+	  });
+	};
+ 
+	//отследит на какой по счёту таб нажал пользователь
+	const showTabContent = (i = 0) => {
+	  contents[i].style.display = 'block';
+	  tabs[i].classList.add(activeClass);
+	};
+ 
+	hideTabContent();
+	showTabContent();
+ 
+	//отследить какой таб кликнул пользователь
+	headers.addEventListener('click', (e) => {
+	  const target = e.target;
+	  //точно ли пользователь кликнул туда, куда надо
+	  //регулярное выражение и то, на что меняем это выражение
+	  //.class => class
+	  if (
+		 target &&
+		 (target.classList.contains(tabSelector.replace(/\./, '')) ||
+			target.parentNode.classList.contains(tabSelector.replace(/\./, '')))
+	  ) {
+		 tabs.forEach((tab, i) => {
+			if (target == tab || target.parentNode == tab) {
+			  hideTabContent();
+			  showTabContent(i);
+			}
+		 });
+	  }
+	});
+ };
+ 
+ export default tabs;
