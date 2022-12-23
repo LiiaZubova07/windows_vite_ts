30461875a0fcce2cedce117c3aa9a26657db6f0d
Liia Zubova <94539297+LiiaZubova07@users.noreply.github.com>
2022-12-23 18:46:00 +0300

    Revert "02 tab handling"

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,7 @@
 import './slider';
-import modals from './modules/modals';
-import tabs from './modules/tabs';
+import modals from './modules/modals.js';
 
+//выполняется тогда, когда ДОМ-структура готова
 window.addEventListener('DOMContentLoaded', () => {
   modals();
-  tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
-  tabs(
-    '.decoration_slider',
-    '.no_click',
-    '.decoration_content > div > div',
-    'after_click'
-  );
 });
