f31480febc611a6eb41d7be450ae8a2de1417f1e
Liia Zubova <vardlitmo@gmail.com>
2022-12-23 19:08:43 +0300

    add: tabs.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,18 @@
 import './slider.js';
 import modals from './modules/modals.js';
-// import tabs from './modules/tabs.js';
+import tabs from './modules/tabs.js';
 import forms from './modules/forms.js';
 
 //выполняется тогда, когда ДОМ-структура готова
 window.addEventListener('DOMContentLoaded', () => {
-	"use strict";
+  'use strict';
   modals();
-
+  tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+  tabs(
+    '.decoration_slider',
+    '.no_click',
+    '.decoration_content > div > div',
+    'after_click'
+  );
   forms();
 });
