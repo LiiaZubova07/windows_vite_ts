90a3ed7396aa1463b7308e4202a0de339a6a594c
Liia Zubova <vardlitmo@gmail.com>
2022-12-23 21:25:44 +0300

    add: tabs

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,28 @@ import modals from './modules/modals.js';
 import tabs from './modules/tabs.js';
 import forms from './modules/forms.js';
 
+const tabsGlazing = () => {
+  '.glazing_slider', '.glazing_block', '.glazing_content', 'active';
+};
+
+const tabsDecoration = () => {
+  '.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click';
+};
+
+// const TabsGlazing = ('.glazing_slider', '.glazing_block', '.glazing_content', 'active') => {};
+
 //выполняется тогда, когда ДОМ-структура готова
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
   modals();
-  tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
-  tabs(
-    '.decoration_slider',
-    '.no_click',
-    '.decoration_content > div > div',
-    'after_click'
-  );
+  //   tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+  tabsGlazing();
+  //   tabs(
+  //     '.decoration_slider',
+  //     '.no_click',
+  //     '.decoration_content > div > div',
+  //     'after_click'
+  //   );
+  tabsDecoration();
   forms();
 });
