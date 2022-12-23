65799d7873171205cb6b6246ba8ecfc416eb59a2
Liia Zubova <vardlitmo@gmail.com>
2022-12-23 18:04:27 +0300

    add: import

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import './slider';
 import modals from './modules/modals.js';
+import tabs from './modules/tabs.js';
+import forms from './modules/forms.js';
 
 //выполняется тогда, когда ДОМ-структура готова
 window.addEventListener('DOMContentLoaded', () => {
   modals();
+//   tabs('')
 });
