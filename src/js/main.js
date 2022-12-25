a2079bc95576729dc1a7ce155f42323312182728
Liia Zubova <vardlitmo@gmail.com>
2022-12-25 21:31:30 +0300

    add: checkNumInputs

--- src/js/main.js
+++ src/js/main.js
@@ -2,27 +2,30 @@ import './slider.js';
 import modals from './modules/modals.js';
 import tabs from './modules/tabs.js';
 import forms from './modules/forms.js';
+import changeModalState from './modules/changeModalState.js';
 
 //выполняется тогда, когда ДОМ-структура готова
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
-const modalState = {};
+  const modalState = {};
 
+  changeModalState();
   modals();
 
   tabs({
-	headerSelector: '.glazing_slider',
-	tabSelector: '.glazing_block',
-	contentSelector: '.glazing_content',
-	activeClass: 'active'});
+    headerSelector: '.glazing_slider',
+    tabSelector: '.glazing_block',
+    contentSelector: '.glazing_content',
+    activeClass: 'active',
+  });
 
-   tabs({
-		headerSelector: '.decoration_slider',
-		tabSelector: '.no_click',
-		contentSelector: '.decoration_content > div > div',
-		activeClass: 'after_click'
-	});
+  tabs({
+    headerSelector: '.decoration_slider',
+    tabSelector: '.no_click',
+    contentSelector: '.decoration_content > div > div',
+    activeClass: 'after_click',
+  });
 
   forms();
 });
--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -1,11 +1,16 @@
-const changeModalState = ()=> {
+const changeModalState = () => {
+  const windowForms = document.querySelectorAll('.balcon_icons_img');
+  const windowWidths = document.querySelector('#width');
+  const windowHeights = document.querySelector('#height');
+  const windowTypes = document.querySelector('#view_type');
+  const windowProfiles = document.querySelectorAll('.checkbox');
 
-const windowForms = document.querySelectorAll('.balcon_icons_img');
-const windowWidths = document.querySelector('#width');
-const windowHeights = document.querySelector('#height');
-const windowTypes = document.querySelector('#view_type');
-const windowProfiles = document.querySelectorAll('.checkbox');
-
+  //когда кликаем на изображение, будет подписана форма, которую выбрал пользователь
+  windowForms.forEach((windowForm, i) => {
+    windowForm.addEventListener('click', () => {
+      state.form = i;
+    });
+  });
 };
 
 export default changeModalState;
