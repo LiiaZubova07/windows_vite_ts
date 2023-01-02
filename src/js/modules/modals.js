a88c70ed7cd548a9a03c86555e156bf4b7593cbc
Liia Zubova <vardlitmo@gmail.com>
2023-01-02 17:37:24 +0300

    add: animation

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -7,6 +7,7 @@ const modals = () => {
     const modal = document.querySelector(modalSelector);
     const close = document.querySelector(closeSelector);
     const windows = document.querySelectorAll('[data-modal]');
+    const scroll = calcScroll();
 
     const closeModal = () => {
       modal.style.display = 'none';
@@ -21,7 +22,6 @@ const modals = () => {
           e.preventDefault();
         }
 
-
         windows.forEach((window) => {
           window.style.display = 'none';
         });
@@ -29,6 +29,7 @@ const modals = () => {
         modal.style.display = 'block';
         //когда модальное окно открыто, то скролится только модальное окно
         document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`;
         //чтоб использовать класс
         // document.body.classList.add('modal-open');
         const input = modal.querySelector('input');
@@ -42,6 +43,7 @@ const modals = () => {
         window.style.display = 'none';
       });
       closeModal();
+      document.body.style.marginRight = `0px`;
       //чтоб использовать класс
       // document.body.classList.remove('modal-open');
     });
@@ -53,6 +55,7 @@ const modals = () => {
           window.style.display = 'none';
         });
         closeModal();
+        document.body.style.marginRight = `0px`;
         //чтоб использовать класс
         // document.body.classList.remove('modal-open');
       }
@@ -62,6 +65,7 @@ const modals = () => {
     document.addEventListener('keydown', (e) => {
       if (e.key === 'Escape') {
         closeModal();
+        document.body.style.marginRight = `0px`;
       }
     });
   }
@@ -72,6 +76,21 @@ const modals = () => {
       document.body.style.overflow = 'hidden';
     }, time);
   };
+
+  function calcScroll() {
+    const div = document.createElement('div');
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    //вычислить размер прокрутки
+    const scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+  }
 
   bindModal({
     triggersSelector: '.popup_engineer_btn',
